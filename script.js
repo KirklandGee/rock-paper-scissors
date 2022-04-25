2736901ff0850c7df3a093a9f18fee64455f22db
Kirkland Gee <kirklandgee12@gmail.com>
2022-04-25 11:12:51 -0400

    Add playGame function that plays RPS 5 times
    When a winner is chosen, the script will log that winner
    and the number of rounds they won to the console.

--- script.js
+++ script.js
@@ -11,6 +11,22 @@ const choices = ["rock", "paper", "scissors"]
 let computerPoints = 0
 let userPoints = 0
 
+// Uses the other functions to play 5 rounds of the game and decide a winner
+function playGame() {
+    for (i=0; i<5; i++) {
+        playRound()
+    }
+
+    if (userPoints > computerPoints) {
+        console.log(`You win :) You won ${userPoints} out of 5 rounds.`)
+    } else {
+        console.log(`You lost :( You only won ${userPoints} out of 5 rounds.`)
+    }
+    userPoints = 0
+    computerPoints = 0
+}
+
+// Takes the random computer choice and user input and compares their choices against a set of options to choose a winner
 function playRound (computerChoice, userChoice) {
     computerChoice = computerPlay()
     userChoice = userPlay()
@@ -23,16 +39,16 @@ function playRound (computerChoice, userChoice) {
         console.log("You lose! :( Paper covers rock")
         userPoints++
     } else if (userChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose! Rock smashes scissors")
+        console.log("You lose! :( Rock smashes scissors")
         computerPoints++
     } else if (userChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! Scissors cuts paper")
+        console.log("You win! :) Scissors cuts paper")
         userPoints++
     } else if (userChoice == "paper" && computerChoice == "rock") {
-        console.log("You win! Paper covers rock")
+        console.log("You win! :) Paper covers rock")
         computerPoints++
     } else if (userChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose! Scissors cuts paper")
+        console.log("You lose! :( Scissors cuts paper")
         computerPoints++
     }
 }
@@ -44,12 +60,14 @@ function computerPlay () {
 
 // Takes in a user input and validates it is either rock, paper, or scissors, case-insensitive.
 function userPlay () {
-    const userChoice = prompt("What do you throw? ")
-    userChoice.toLowerCase()
-    if (!choices.includes(userChoice)) {
+    let userChoice = prompt("What do you throw? ")
+    userChoice = userChoice.toLowerCase()
+    while (!choices.includes(userChoice)) {
         console.log("That is not a valid play. Please throw rock, paper, or scissors")
         userChoice = prompt("Please throw again: ")
-    } else {
-        return userChoice
-    }
+    } 
+    
+    return userChoice
 }
+
+playGame()
